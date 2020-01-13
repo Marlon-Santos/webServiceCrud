9da47f3d911b368ba4ba7450d94f13f3640be6c5
marlon-santos <marlonergondossantos@gmail.com>
2020-01-13 11:20:07 -0300

    add RestController GetMappin

--- src/main/java/br/com/gft/learnSpring/resources/UserResources.java
+++ src/main/java/br/com/gft/learnSpring/resources/UserResources.java
@@ -1,4 +1,17 @@
 package br.com.gft.learnSpring.resources;
 
+import br.com.gft.learnSpring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResources {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "marlon ergon dos santos", "marlonergondossantos@gmail.com", "4195490128", "egvtest");
+        return ResponseEntity.ok().body(u);
+    }
 }
